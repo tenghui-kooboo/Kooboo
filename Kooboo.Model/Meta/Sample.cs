cfbe72fbb1582786b8dd18dbf184a9ac7d511119
suning <suningbiz@qq.com>
2019-05-22 18:41:16 +0800

    add meta sample

---
+++ Kooboo.Model/Meta/Sample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kooboo.Model.Meta.Form;
+using Kooboo.Model.Meta.Table;
+
+namespace Kooboo.Model.Meta
+{
+    class Model
+    {
+        public string Name { get; set; }
+    }
+
+    class Sample : IMetaConfigure<Model, Table.TableMeta>, IMetaConfigure<Model, Form.FormMeta>
+    {
+        public void Configure(TableMeta meta)
+        {
+        }
+
+        void IMetaConfigure<Model, FormMeta>.Configure(FormMeta meta)
+        {
+            meta.Builder<Model>()
+                .MergeModel();
+        }
+    }
+
+
+
+}
